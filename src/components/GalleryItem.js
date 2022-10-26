6b305faeac561fcb3b23b048f739ff03b5c7aac2
Simon Franco <simonfranco0@gmail.com>
2022-10-25 17:27:40 -0700

    homework done

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,8 +1,8 @@
 
 
-import React, { useState } from 'react'
+import React from 'react'
+import { useState } from 'react' 
 import { Link } from 'react-router-dom'
-// import {Link} from 'react-router-dom'
 
 function GalleryItem(props){
     let [view, setView] = useState(false)
@@ -12,14 +12,6 @@ function GalleryItem(props){
         'height': '20vh',
         'border': '1px solid black',
         'margin': '2px'
-    }
-    const simpleView = () => {
-        return (
-            <div style={simpleStyle}>
-                <h3>{props.item.trackName}</h3>
-                <h4>{props.item.collectionName}</h4>
-            </div>
-        )
     }
     
     const detailStyle = {
@@ -32,17 +24,27 @@ function GalleryItem(props){
         'backgroundSize': 'cover',
         'color': 'yellow'
     }
+
+    const simpleView = () => {
+        return (
+            <div style={simpleStyle}>
+                <h3>{props.item.trackName}</h3>
+                <h4>{props.item.collectionName}</h4>
+            </div>
+        )
+    }
+
     const detailView = () => {
         return (
             <div style={detailStyle}>
                 <h2>{props.item.trackName}</h2>
                 <h3>
-                    <Link to={`/artist/${props.artistId}`}>
+                    <Link to={`/artist/${props.item.artistId}`}>
                         {props.item.artistName}
                     </Link>
                 </h3>
                 <h3>
-                    <Link to={`/album/${props.collectionId}`}>
+                    <Link to={`/album/${props.item.collectionId}`}>
                         {props.item.collectionName}
                     </Link>
                 </h3>
@@ -51,7 +53,7 @@ function GalleryItem(props){
             </div>
         )
     }
-    
+
     return (
         <div onClick={() =>setView(!view)} style={{'display': 'inline-block'}}>
             {/* This simple ternary shows the simple view when 'view' is false! */}
